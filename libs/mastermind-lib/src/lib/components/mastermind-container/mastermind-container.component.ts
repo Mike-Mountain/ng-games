95208860e596e487a3ae7a8d75368a35d1a03983
Michael <mikemountain.dev@gmail.com>
2022-03-31 22:14:27 +0200

    Fix toolbar service not updating title

--- libs/mastermind-lib/src/lib/components/mastermind-container/mastermind-container.component.ts
+++ libs/mastermind-lib/src/lib/components/mastermind-container/mastermind-container.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Colors, colors, createMsmGame } from '../../models/mastermind.model';
 import { MastermindService } from '../../services/mastermind.service';
 import { WinState } from '../../models/win-state.enum';
+import { ToolbarService } from '@ng-games/container-lib';
 
 @Component({
   selector: 'msm-mastermind-container',
@@ -16,9 +17,13 @@ export class MastermindContainerComponent implements OnInit, AfterViewInit {
   public state: WinState = WinState.InProgress;
   public WinsState = WinState;
 
-  constructor(private mastermindService: MastermindService) {}
+  constructor(
+    private mastermindService: MastermindService,
+    private toolbarService: ToolbarService
+  ) {}
 
   ngOnInit(): void {
+    this.toolbarService.updateTitle('Code Master');
     this.computerColors = this.mastermindService.selectComputerColors();
     this.mastermindService.turns = createMsmGame(10);
   }
