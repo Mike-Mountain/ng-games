eb1d66097dd6936ed92af53f9028417bf6262437
Michael <mikemountain.dev@gmail.com>
2022-03-31 22:33:49 +0200

    Fix no provider for matDialog

--- apps/container/src/app/app.module.ts
+++ apps/container/src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CtrLayoutModule } from '@ng-games/container-lib';
+import { CtrLayoutModule, ToolbarService } from '@ng-games/container-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatDialogModule,
     AppRoutingModule,
     CtrLayoutModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/mastermind-lib/src/lib/mastermind-lib.module.ts
+++ libs/mastermind-lib/src/lib/mastermind-lib.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MastermindContainerComponent } from './components/mastermind-container/mastermind-container.component';
 import { RouterModule, Routes } from '@angular/router';
 import { GameBoardComponent } from './components/game-board/game-board.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { ToolbarService } from '@ng-games/container-lib';
 
 const routes: Routes = [{ path: '', component: MastermindContainerComponent }];
 
@@ -18,5 +19,12 @@ const routes: Routes = [{ path: '', component: MastermindContainerComponent }];
     MatCardModule,
   ],
   declarations: [MastermindContainerComponent, GameBoardComponent],
+  providers: [
+    {
+      provide: MatDialogRef,
+      useValue: {},
+    },
+    ToolbarService,
+  ],
 })
 export class MastermindLibModule {}
